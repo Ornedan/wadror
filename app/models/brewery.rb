4a51577f6a1bb35d4631da13ab9bc775c575fd3f
Heikki Aitakangas <ornedan@gmail.com>
2014-01-26 20:34:50 +0200

    Up to exercise 2.14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,8 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  def average_rating
+    self.ratings.pluck(:score).sum
+  end
 end
