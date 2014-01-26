5bba2a7c02ee5a514025c127d2e3cbd799c17c49
Heikki Aitakangas <ornedan@gmail.com>
2014-01-26 20:29:27 +0200

    Up to exercise 2.13

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,8 +1,12 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
-  has_many :ratings
+  has_many :ratings, dependent: :destroy
 
   def average_rating
     self.ratings.pluck(:score).sum
+  end
+
+  def to_s
+    self.name + " by " + self.brewery.name
   end
 end
