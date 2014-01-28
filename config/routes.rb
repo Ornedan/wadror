b49a0854da44aa1bcc1c2e3caf2cbda0177c3910
Heikki Aitakangas <ornedan@gmail.com>
2014-01-28 17:31:25 +0200

    Up to exercise 3.2

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Ratebeer::Application.routes.draw do
 
   resources :ratings, only: [:index, :new, :create, :destroy]
 
+  resources :users
+  get 'signup', to: 'users#new'
+
+  resources :sessions, only: [:new, :create]
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
