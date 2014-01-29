e1c02094c11d0ce31ca1d289ab9f6d10bc5cb4cc
Heikki Aitakangas <ornedan@gmail.com>
2014-01-29 19:08:25 +0200

    Up to exercise 3.6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -6,7 +6,12 @@ class Brewery < ActiveRecord::Base
 
   validates :name, presence: true
   validates :year, numericality: { greater_than_or_equal_to: 1042,
-                                    less_than_or_equal_to: 2014,
-                                    only_integer: true }
+                                   only_integer: true }
+  validate :not_in_future
 
+  def not_in_future
+    if year > Time.now.year
+      errors.add(:year, "must be less than or equal to #{Time.now.year}")
+    end
+  end
 end
